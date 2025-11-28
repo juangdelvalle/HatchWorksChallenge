dadf7c3b2705b2df6a67d0231ca1700c95d9cee6
Juan del Valle <juangdelvalle@gmail.com>
2025-11-28 11:32:54 -0500

    Initial setup of the project

---
+++ HatchWorksChallenge/HatchWorksChallenge/HatchWorksChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchWorksChallengeApp.swift
+//  HatchWorksChallenge
+//
+//  Created by Juan del Valle Ruiz on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HatchWorksChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HatchWorksChallenge/HatchWorksChallenge/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HatchWorksChallenge
+//
+//  Created by Juan del Valle Ruiz on 11/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
